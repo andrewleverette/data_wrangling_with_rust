904dc19cdb973f4da02e1ae5e38b4b495b1b3d8a
Andrew Leverette <andrewleverette@gmail.com>
2021-03-15 20:51:34 -0500

    Add comments to dataframe example

--- data_aggregation_with_datafusion/datafusion_dataframe_example/src/main.rs
+++ data_aggregation_with_datafusion/datafusion_dataframe_example/src/main.rs
@@ -3,12 +3,26 @@ use datafusion::prelude::*;
 
 #[tokio::main]
 async fn main() -> DataFusionResult<()> {
+    // Initialize query interface
     let mut ctx = ExecutionContext::new();
 
+    // Creates a data frame that describes a query to scan a  CSV file,
+    // calculate the average of each score by group,
+    // then finally sort by group.
     let df = ctx
         .read_csv("../../data/StudentACTResults.csv", CsvReadOptions::new())?
-        .aggregate(vec![col("group")], vec![avg(col("english"))])?;
+        .aggregate(
+            vec![col("group")],
+            vec![
+                avg(col("english")),
+                avg(col("reading")),
+                avg(col("math")),
+                avg(col("science")),
+            ],
+        )?
+        .sort(vec![col("group").sort(true, true)])?;
 
+    // Execute the query defined by the data frame
     let results = df.collect().await?;
 
     println!("{:?}", results);
